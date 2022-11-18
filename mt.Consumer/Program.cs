42737e4f330a37e0d19423b9f86738c8eaada81b
Marcin Węglowski <mawg@softwaremind.com>
2022-11-18 23:03:05 +0100

    Add mt.consumer

---
+++ mt.Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = new HostBuilder()
+    .ConfigureServices(services =>
+    {
+    })
+    .ConfigureLogging((hostingContext, logging) => {
+        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        logging.AddConsole();
+    });
+
+builder.RunConsoleAsync();
